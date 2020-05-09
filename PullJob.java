65fc54ee67d0b67b60c439ade02980eabd1aedd9
sumitpaul93 <65072393+sumitpaul93@users.noreply.github.com>
2020-05-09 18:46:34 +0530

    Incorporated webhook

--- PullJob.java
+++ PullJob.java
@@ -2,7 +2,7 @@ public class PullJob
 {
 	public static void main(String[] args){
 		{
-			System.out.println("Running pullJob version 2.0");
+			System.out.println("Running pullJob version 3.0");
 		}
 	}
 }
